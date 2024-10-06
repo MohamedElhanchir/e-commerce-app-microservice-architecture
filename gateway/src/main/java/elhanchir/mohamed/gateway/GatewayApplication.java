661aece414725b9e875c578c4eade1d36e525abe
Mohamed Elhanchir <elhanchirmohamed349@gmail.com>
2024-10-06 12:44:16 +0100

    Static routes configuration - discovry enabled

--- gateway/src/main/java/elhanchir/mohamed/gateway/GatewayApplication.java
+++ gateway/src/main/java/elhanchir/mohamed/gateway/GatewayApplication.java
@@ -16,8 +16,8 @@ public class GatewayApplication {
     @Bean
     RouteLocator routes(RouteLocatorBuilder builder){
         return builder.routes()
-                .route(r->r.path("/customers/**").uri("http://localhost:8081/"))
-                .route(r->r.path("/products/**").uri("http://localhost:8082/"))
+                .route(r->r.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
+                .route(r->r.path("/products/**").uri("lb://INVENTORY-SERVICE"))
                 .build();
     }
 
